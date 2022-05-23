bac245d945a631c1fbf4ed4881d00aff278b7b7c
Buğra Aydoğar <bugra.aydogar@canonical.com>
2022-05-23 21:17:12 +0300

    Added refresh in every 50 sec and reverts if battery level is bigger than %100

--- app/src/App.vue
+++ app/src/App.vue
@@ -11,21 +11,28 @@
               <td> <h3> Battery </h3> </td>
             </tr>
             <tr>
-              <td><ve-progress :progress="50" text-align="center"/></td>
+              <td><ve-progress :progress="battery" text-align="center"/></td>
             </tr>
           </table>
         </div>
       </form>
       
     </div>
+  <div>
+    <vue-final-modal v-model="showModal" classes="modal-container" content-class="modal-content">
+      <span class="modal__title">Internal System Error Detected</span>
+      <div class="modal__content">
+        <p>Battery Level can not be more %100, Please wait while reverting to the last known good state</p>
+      </div>
+    </vue-final-modal>
+  </div>
   </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader.vue"
 import AppCar from './components/AppCar.vue';
-
-
+import axios from "axios"
 
 export default {
   name: 'App',
@@ -37,8 +44,59 @@ export default {
     return {
       title: "Ubuntu Core Over the Air Update",
       wheels: 19,
-      speed: 55
+      speed: 55,
+      polling_battery: null,
+      polling_refresh: null,
+      battery: 0,
+      showModal: false
     }
+  },
+  created () {
+    this.loadData()
+    this.pollBattery()
+    this.pollUpdate()
+  },
+  methods: {
+    pollBattery () {
+      this.polling_battery = setInterval(() => {
+        this.loadData()
+      }, 50000)
+    },
+    pollUpdate() {
+      this.polling_refresh = setInterval(() => {
+        this.refresh()
+      }, 50000)
+
+    },
+    async loadData() {
+      console.log("Load Data is called")
+      const res = await axios.get("http://localhost:8000/battery-level")
+      this.battery = res.data.battery
+      
+      if(this.battery > 100) {
+        showModal = true
+        await this.revert()
+        showModal = false
+        const res = await axios.get("http://localhost:8000/battery-level")
+        this.battery = res.data.battery
+      }
+
+    },
+    async refresh() {
+      console.log("refresh is called")
+      const res = await axios.post("http://localhost:8000/refresh")
+      console.log(res)
+    },
+    async revert() {
+      console.log("Revert is called...")
+      const payload = ['snapd']
+      const res = await axios.post("http://localhost:8000/revert", payload)
+    }
+  },
+  beforeDestroy () {
+    clearInterval(this.polling_battery)
+    clearInterval(this.polling_refresh)
+
   }
 }
 </script>
@@ -46,4 +104,32 @@ export default {
 
 <style lang="scss">
 @import 'tesla-style.scss';
+</style>
+
+<style scoped>
+::v-deep .modal-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+::v-deep .modal-content {
+  display: flex;
+  flex-direction: column;
+  margin: 0 1rem;
+  padding: 1rem;
+  border: 1px solid #e2e8f0;
+  border-radius: 0.25rem;
+  background: #fff;
+}
+.modal__title {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+</style>
+
+<style scoped>
+.dark-mode div::v-deep .modal-content {
+  border-color: #2d3748;
+  background-color: #1a202c;
+}
 </style>
--- app/src/main.js
+++ app/src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import veProgress from "vue-ellipse-progress";
+import { vfmPlugin } from 'vue-final-modal'
 
 
 const app = createApp(App)
 
 app.use(router)
 app.use(veProgress);
-
+app.use(vfmPlugin);
 
 app.mount('#app')
