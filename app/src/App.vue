4f6308c15b8139ed743c1279bb507a189bcdfef2
Buğra Aydoğar <bugraaydogar@gmail.com>
2022-06-15 00:13:56 +0300

    Replaced the circle progress bar with a speedometer

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,21 +2,40 @@
   <div id="app">
     <app-header/>
     <div class="wrapper">
-      <form class="tesla-battery">
+      <form class="tesla-car-container">
         <h1>{{ title }}</h1>
         <app-car :wheelsize="wheels" :speed="speed" />
         <div>
           <table>
             <tr>
-              <td> <h3> Current Speed </h3> </td>
+              <td> <h3> Speed </h3> </td>
             </tr>
             <tr>
-              <td><ve-progress :progress="battery" text-align="center"/></td>
+              <td>
+                <div id="gauge-demo">
+                  <div id="gauge-container">
+                    <div class="center-section">
+                      <DxCircularGauge :value="car_speed">
+                        <DxSize :width="260"/>
+                        <DxValueIndicator
+                          :second-fraction="0.24"
+                          type="twoColorNeedle"
+                          color="none"
+                          second-color="#f05b41"/>
+                        <DxGeometry :start-angle="225" :end-angle="315" />
+                        <DxScale :start-value="0" :end-value="180" :tick-interval="20" :minor-tick-interval="10" />
+                      </DxCircularGauge>
+                      <div class="speed-value">
+                        <span>{{ car_speed }}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
             </tr>
           </table>
         </div>
       </form>
-
     </div>
   <div>
     <vue-final-modal v-model="showModal" classes="modal-container" content-class="modal-content">
@@ -26,39 +45,73 @@
       </div>
     </vue-final-modal>
   </div>
-  </div>
+
+</div>
+
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader.vue"
 import AppCar from './components/AppCar.vue';
 import axios from "axios"
+import DxSlider from 'devextreme-vue/slider';
+import DxCircularGauge, {
+  DxSize,
+  DxValueIndicator,
+  DxGeometry,
+  DxScale,
+} from 'devextreme-vue/circular-gauge';
+import DxLinearGauge, {
+  DxSize as DxLinearSize,
+  DxValueIndicator as DxLinearValueIndicator,
+  DxScale as DxLinearScale,
+  DxMinorTick,
+  DxLabel,
+} from 'devextreme-vue/linear-gauge';
+
 
 export default {
   name: 'App',
   components: {
     AppHeader,
     AppCar,
+    DxSlider,
+
+    DxCircularGauge,
+    DxSize,
+    DxValueIndicator,
+    DxGeometry,
+    DxScale,
+
+    DxLinearGauge,
+    DxLinearSize,
+    DxLinearValueIndicator,
+    DxLinearScale,
+    DxMinorTick,
+    DxLabel,
+
   },
   data() {
     return {
       title: "Ubuntu Core Over the Air Update",
       wheels: 19,
+      max_allowed_speed: 100,
+      current_max_speed: 0,
       speed: 55,
-      polling_battery: null,
+      polling_speed: null,
+      car_speed: 0,
       polling_refresh: null,
-      battery: 0,
-      showModal: false
+      showModal: false,
     }
   },
   created () {
     this.loadData()
-    this.pollBattery()
+    this.pollSpeed()
     this.pollUpdate()
   },
   methods: {
-    pollBattery () {
-      this.polling_battery = setInterval(() => {
+    pollSpeed () {
+      this.polling_speed = setInterval(() => {
         this.loadData()
       }, 500)
     },
@@ -70,35 +123,35 @@ export default {
     },
     async loadData() {
       // console.log("Load Data is called")
-      const res = await axios.get("http://localhost:3000/battery-level")
+      const res = await axios.get("http://localhost:3000/speed-level")
       // Use the battery value for the speed as well
-      this.speed = this.battery = res.data.battery
+      this.car_speed = res.data.speed
+      this.current_max_speed = res.data.maxSpeed
 
-      if(this.battery > 100) {
+      if(this.current_max_speed > this.max_allowed_speed) {
         this.showModal = true
         await this.revert()
-        const res = await axios.get("http://localhost:3000/battery-level")
-        this.battery = res.data.battery
+        const res = await axios.get("http://localhost:3000/speed-level")
+        this.car_speed = res.data.speed
+        this.current_max_speed = res.data.maxSpeed
       }
 
-      if (this.battery < 100) {
+      if (this.current_max_speed < this.max_allowed_speed) {
         this.showModal = false
       }
 
     },
     async refresh() {
-      console.log("refresh is called")
       const res = await axios.post("http://localhost:5000/refresh")
       console.log(JSON. stringify(res))
     },
     async revert() {
-      console.log("Revert is called...")
       const res = await axios.post("http://localhost:5000/revert", {"name": "iotdevice-device-controller"})
       console.log(JSON. stringify(res))
     }
   },
   beforeDestroy () {
-    clearInterval(this.polling_battery)
+    clearInterval(this.polling_speed)
     clearInterval(this.polling_refresh)
 
   }
